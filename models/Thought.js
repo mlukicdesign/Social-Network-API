14c780b42f3df0ac4601b9666655499dc9f01974
mlukicdesign <mlukicdesign@gmail.com>
2023-07-21 11:29:48 +0800

    Initial commit & Created API Models

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughText: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date, // insert date format
+            default: Date.now(),
+            get: (createdAt) => new Date(createdAt).toISOString(),
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: { virtuals: true },
+        id: false,
+    }
+)
+
+// Virtual to get the reactionCount
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+const Thoughts = model('thoughts', thoughtSchema);
+
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        },
+        thoughts: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought',
+        }],
+        friends: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+    },
+    {
+        toJSON: { virtuals: true },
+        id: false,
+      }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
